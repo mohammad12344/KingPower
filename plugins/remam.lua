55ecdf8e06f6a772dfcc62f82ab68ca8e0e47b00
mohammad12344 <sdf12345641@gmail.com>
2016-03-22 13:29:14 +0430

    Update remam.lua

--- plugins/remam.lua
+++ plugins/remam.lua
@@ -2,7 +2,7 @@ do local function run(msg, matches) if not is_admin(msg) then return nil end
 
 if msg.to.type == 'chat'then
 
-if matches[1] == "حذف ادمین" then Channel = 'chat#'..msg.to.id user = 'user#'..matches[2] chat_rem_admin(Chat, user, ok_cb, true) return  کاربر..'\nاز ادمینی بات خارج شد end
+if matches[1] == "حذف ادمین" then Channel = 'chat#'..msg.to.id user = 'user#'..matches[2] chat_rem_admin(Chat, user, ok_cb, true) return  user..'\nاز ادمینی بات خارج شد end
 
 end end  return { description = "", usage = {}, patterns = { "^(حذف ادمین) (%d+)$" }, run = run }
 
