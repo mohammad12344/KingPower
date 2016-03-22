9501b7f054ff31249d8824977fcdafd7886501e6
mohammad12344 <sdf12345641@gmail.com>
2016-03-22 13:28:25 +0430

    Update addam.lua

--- plugins/addam.lua
+++ plugins/addam.lua
@@ -2,7 +2,7 @@ do  local function run(msg, matches) if not is_admin(msg) then return nil end
 
 if msg.to.type == 'chat'then
 
-if matches[1] == "اضافه ادمین" then Channel = 'chat#'..msg.to.id user = 'user#'..matches[2]chda_set_admin(chat, user, ok_cb, true) return user..'\nadd as admin' end
+if matches[1] == "اضافه ادمین" then Channel = 'chat#'..msg.to.id user = 'user#'..matches[2]chda_set_admin(chat, user, ok_cb, true) return user..'\nبه ادیمینی بات اضافه شد' end
 
 end 
 end  
